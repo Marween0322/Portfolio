f1296857642f4e19a86b889ebcad08e13b5e2a06
Marween0322 <marweenomingo03@gmail.com>
2023-11-09 18:06:22 +0800

    added the design of mobile

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+
 // JavaScript to add the 'scrolled' class to the navbar when scrolling
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('nav');
-    if (window.scrollY > 50) {
+    const mobileNav = document.querySelector('nav.mobile-nav');
+
+    if (window.scrollY > 10) {
         navbar.classList.add('scrolled');
+        mobileNav.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
+        mobileNav.classList.remove('scrolled');
     }
 });
