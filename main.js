875a91830432f47f1201395f802135873f4fac0a
Marween0322 <marweenomingo03@gmail.com>
2023-11-17 13:11:10 +0800

    any comment

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 // JavaScript to add the 'scrolled' class to the navbar when scrolling
     const navbar = document.querySelector('nav');
     const mobileNav = document.querySelector('nav.mobile-nav');
@@ -26,3 +27,19 @@ menuIcon.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
     mobileMenuContainer.classList.remove('active');
 });
+
+// JavaScript for smooth scrolling to Skills section
+function scrollToSection(linkId, sectionId) {
+    document.getElementById(linkId).addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log(`${linkId} link clicked`);
+        document.getElementById(sectionId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}
+
+// Usage
+scrollToSection('about-link', 'about-section');
+scrollToSection('skills-link', 'skills-section');
+scrollToSection('myprojects-link', 'myprojects-section');
